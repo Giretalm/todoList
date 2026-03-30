552f9c575b1cbb93c163bf4cbe347ea4d5390dc6
giretalm <giretalm@yahoo.com>
2026-03-30 18:17:53 -0400

    inital commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from 'react';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ },
 });
+
+export default function App() {
+  let data = [
+   { 
+      key: "1", 
+      title: "Title1"
+   },
+    { key: "2", 
+      title: "Title2"
+   },
+  ];
+
+  let renderItem = ({ item }) => {
+    return <Text>{item.title}</Text>;
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.key}
+      />
+    </View>
+ );
+}
